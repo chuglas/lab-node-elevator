59c0ffb0522f17f0b80fe37825ac4d971502a3f1
chuglas <doug@wearecirro.com>
2017-02-06 19:44:27 +0100

    working on step 3

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,84 @@
+var counter;
+const Person = require('./person.js');
+
+
+
 class Elevator {
   constructor(){
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.direction  = 'up';
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+    //calls update every second to prompt the execution
+    counter = setInterval(() => this.update(), 1000);
+  }
+
+  stop() {
+    //ceases the elevator's listening for requests
+    console.log("stop it!");
+    clearInterval(counter);
+  }
+
+  update() {
+    //display in the console the actual status of the elevator by calling the log function
+    this.log();
+    this.floorUp();
+    //List of floors that the elevator should serve.
+  }
+
+  _passengersEnter() {
+      // add their floor to the destinationFloors
+      // let queue = this.requests;
+      // queue.push(Person.destinationFloor);
+      // queue.sort();
+  }
+
+  _passengersLeave() {
+      // remove their floor from the destinationFloors
+  }
+
+  floorUp(requestFloor) {
+    if (this.floor < requestFloor) {
+      this.floor = this.floor + 1;
+    }
+  }
+
+  floorDown() {
+    if (this.floor > 0) {
+      this.floor = this.floor - 1;
+    }
+  }
+
+  call() {
+    // call method. This method should add a request into
+    // the elevator’s queue. A good idea would be to add
+    // the whole person object to the requests collection.
+    this.requests.push(Doug.originFloor);
+    console.log(this.requests);
+    console.log("callinngggg")
+    if (this.floor < Doug.originFloor) {
+      this.floorUp(Doug.originFloor);
+    }
+    else if (this.floor < Doug.originFloor) {
+      this.floorDown();
+    }
+  }
+
+  log() {
+    console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
+  }
 }
 
 module.exports = Elevator;
+
+var hotelCalifornia = new Elevator();
+var Doug = new Person("Doug", 3, 7);
+
+
+hotelCalifornia.start();
+hotelCalifornia.call();
+
+// setTimeout(function(){ hotelCalifornia.stop()},  5000);
--- starter_code/person.js
+++ starter_code/person.js
@@ -1,6 +1,11 @@
 class Person {
   constructor(name, originFloor, destinationFloor){
+    this.name = name;
+    this.originFloor = originFloor;
+    this.destinationFloor = destinationFloor;
   }
 }
 
 module.exports = Person;
+
+var Doug = new Person("Doug", 3, 7);
